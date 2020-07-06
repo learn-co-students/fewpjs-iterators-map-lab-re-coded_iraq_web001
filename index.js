fc47940413aab66db377c09c97157a84688efb35
Sara Nooraldin <sarasaadiq98@gmail.com>
2020-07-06 14:37:16 +0000

    Done.

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const tutorials = [
 function titleCased() {
   return tutorials.map(function (tutorial) {
     tutorial = tutorial.split(' ');
-    for (var i = 0; i < tutorial.length; i++) {
+    for (let i = 0; i < tutorial.length; i++) {
       tutorial[i] = tutorial[i].charAt(0).toUpperCase() + tutorial[i].slice(1);
     }
     return tutorial.join(' ');
